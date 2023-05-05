183288ce4650f4b3e277242ba66fc5ee6d7901e0
shubham-1903 <shubhamkr7677@gmail.com>
2023-05-05 21:24:31 +0530

    port added in env

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,12 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import productsRouter from './routes/products.js';
 import { connectDB } from './db/connect.js';
 dotenv.config()
 const app = express();
-const port = 5500
+
 app.use(bodyParser.json());
 app.use(cors())
 // mongoose.connect('mongodb://localhost:27017/webinardb').then(()=>{
@@ -20,6 +19,6 @@ app.get('/', (req, res)=>{
 })
 app.use("/api/products",productsRouter);
 connectDB(process.env.MONGODB_URL);
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server started on port`);
 });
